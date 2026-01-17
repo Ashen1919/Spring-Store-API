965e5e13c9fe6b5a0b0efa568e1c5e6bea62de66
Ashen1919 <2303S0834@saegis.ac.lk>
2026-01-17 22:12:02 +0530

    Update code

--- src/main/java/com/store_api/springstoreapi/Controllers/UserController.java
+++ src/main/java/com/store_api/springstoreapi/Controllers/UserController.java
@@ -5,11 +5,11 @@ import com.store_api.springstoreapi.Mappers.UserMapper;
 import com.store_api.springstoreapi.Repositories.UserRepository;
 import com.store_api.springstoreapi.dtos.UserDto;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -19,9 +19,13 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
+    public Iterable<UserDto> getAllUsers(
+            @RequestParam(required = false, defaultValue = "", name = "sort") String sort
+    ) {
+        if(!Set.of("name", "email").contains(sort))
+            sort = "name";
 
-        return userRepository.findAll()
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(userMapper :: toDto)
                 .toList();
